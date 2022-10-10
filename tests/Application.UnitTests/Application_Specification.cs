d84b7a561b83d33d9b18bf8f9e3e82e4417c7d85
Johannes Prinz <johannes.prinz@valocity.co.nz>
2022-10-10 23:51:11 +0000

    playing with test failure output in docker multistage build

--- tests/Application.UnitTests/Application_Specification.cs
+++ tests/Application.UnitTests/Application_Specification.cs
@@ -4,4 +4,14 @@ public class Application_Specification
 {
     [Fact]
     public void This_is_a_passing_test() => true.Should().BeTrue();
+
+    [Fact]
+    public void This_is_another_passing_test() => true.Should().BeTrue();
+
+    // The docker pipeline exits on first test failure
+    // Is there a way to run all tests and get all failures reported on
+    // similar to how ncrunch might do it or a ci pipeline
+    // Todo: research test runner settings
+    [Fact(Skip = "This is to test failing output in a docker pipeline")]
+    public void This_is_a_failing_test() => false.Should().BeTrue();
 }
